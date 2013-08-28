d8ab11ec827e820f70af09f71fbe0edcda42942f
Rich Humphrey <rdh727@gmail.com>
2013-08-27 17:21:37 -0700

    Fixed test failure introduced by load order change upgrading from Rails 3.2.8 -> 3.2.14

--- test/dummy/config/initializers/status_cat.rb
+++ test/dummy/config/initializers/status_cat.rb
@@ -1,5 +1,4 @@
 StatusCat.configure do |config|
-  config.enabled = [ :action_mailer, :active_record ]
   config.noreply = 'noreply@schrodingersbox.com'
   config.to = 'ops@schrodingersbox.com'
   config.from = 'ops@schrodingersbox.com'
